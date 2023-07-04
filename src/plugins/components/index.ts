189bd4a58e0b41239205b72dcb385672c74a33e4
BIYA Paul <bpsmartdesign@hotmail.com>
2023-07-04 08:54:57 +0100

    feat: button component

--- src/plugins/components/index.ts
+++ src/plugins/components/index.ts
@@ -1,6 +1,7 @@
 import plugin from 'tailwindcss/plugin'
 import { defu } from 'defu'
 import { type PluginOption } from '../options'
+import button from './button'
 import dropdown from './dropdown'
 import focus from './focus'
 import label from './label'
@@ -11,6 +12,7 @@ import slimscroll from './slimscroll'
 import tooltip from './tooltip'
 
 const components = [
+  button,
   dropdown,
   focus,
   label,
