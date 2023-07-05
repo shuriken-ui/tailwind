0c77650babf53c09e3c2a814d872f518e60a3593
BIYA Paul <bpsmartdesign@hotmail.com>
2023-07-05 17:52:50 +0100

    feat: handle text

---
+++ src/plugins/components/text.ts
@@ -0,0 +1,128 @@
+import plugin from 'tailwindcss/plugin'
+import { defu } from 'defu'
+import { type PluginOption, defaultPluginOptions } from '../options'
+
+const defaultTextConfig = {
+  textXS: 'xs',
+  textSM: 'sm',
+  textMD: 'base',
+  textLG: 'lg',
+  textXL: 'xl',
+  text2XL: '2xl',
+  text3XL: '3xl',
+  text4XL: '4xl',
+  text5XL: '5xl',
+  text6XL: '6xl',
+  text7XL: '7xl',
+  text8XL: '8xl',
+  text9XL: '9xl',
+  textLight: 'light',
+  textNormal: 'normal',
+  textMedium: 'medium',
+  textSemibold: 'semibold',
+  textBold: 'bold',
+  textExtrabold: 'extrabold',
+  textLeadNone: 'none',
+  textLeadNormal: 'normal',
+  textLeadTight: 'tight',
+  textLeadSnug: 'snug',
+  textLeadLoose: 'loose',
+}
+
+export default plugin.withOptions(
+  function (options: PluginOption) {
+    const { prefix } = defu(options, defaultPluginOptions)
+
+    return function ({ addComponents, theme }) {
+      const config = theme('shurikenUi.text') satisfies typeof defaultTextConfig
+
+      addComponents({
+        [`.${prefix}-text`]: {
+          [`@apply font-sans`]: {},
+
+          [`&.${prefix}-content-xs`]: {
+            [`@apply ${config.textXS}`]: {},
+          },
+          [`&.${prefix}-content-sm`]: {
+            [`@apply ${config.textSM}`]: {},
+          },
+          [`&.${prefix}-content-md`]: {
+            [`@apply ${config.textMD}`]: {},
+          },
+          [`&.${prefix}-content-lg`]: {
+            [`@apply ${config.textLG}`]: {},
+          },
+          [`&.${prefix}-content-xl`]: {
+            [`@apply ${config.textXL}`]: {},
+          },
+          [`&.${prefix}-content-2xl`]: {
+            [`@apply ${config.text2XL}`]: {},
+          },
+          [`&.${prefix}-content-3xl`]: {
+            [`@apply ${config.text3XL}`]: {},
+          },
+          [`&.${prefix}-content-4xl`]: {
+            [`@apply ${config.text4XL}`]: {},
+          },
+          [`&.${prefix}-content-5xl`]: {
+            [`@apply ${config.text5XL}`]: {},
+          },
+          [`&.${prefix}-content-6xl`]: {
+            [`@apply ${config.text6XL}`]: {},
+          },
+          [`&.${prefix}-content-7xl`]: {
+            [`@apply ${config.text7XL}`]: {},
+          },
+          [`&.${prefix}-content-8xl`]: {
+            [`@apply ${config.text8XL}`]: {},
+          },
+          [`&.${prefix}-content-9xl`]: {
+            [`@apply ${config.text9XL}`]: {},
+          },
+          [`&.${prefix}-weight-light`]: {
+            [`@apply ${config.textLight}`]: {},
+          },
+          [`&.${prefix}-weight-normal`]: {
+            [`@apply ${config.textNormal}`]: {},
+          },
+          [`&.${prefix}-weight-medium`]: {
+            [`@apply ${config.textMedium}`]: {},
+          },
+          [`&.${prefix}-weight-semibold`]: {
+            [`@apply ${config.textSemibold}`]: {},
+          },
+          [`&.${prefix}-weight-bold`]: {
+            [`@apply ${config.textBold}`]: {},
+          },
+          [`&.${prefix}-weight-extrabold`]: {
+            [`@apply ${config.textExtrabold}`]: {},
+          },
+          [`&.${prefix}-lead-none`]: {
+            [`@apply ${config.textLeadNone}`]: {},
+          },
+          [`&.${prefix}-lead-normal`]: {
+            [`@apply ${config.textLeadNormal}`]: {},
+          },
+          [`&.${prefix}-lead-tight`]: {
+            [`@apply ${config.textLeadTight}`]: {},
+          },
+          [`&.${prefix}-lead-snug`]: {
+            [`@apply ${config.textLeadSnug}`]: {},
+          },
+          [`&.${prefix}-lead-loose`]: {
+            [`@apply ${config.textLeadLoose}`]: {},
+          },
+        },
+      })
+    }
+  },
+  function () {
+    return {
+      theme: {
+        shurikenUi: {
+          text: defaultTextConfig,
+        },
+      },
+    }
+  }
+)
