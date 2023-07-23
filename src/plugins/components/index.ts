2e8adc3e98ee11ab9521a91abcbd14c964592868
Biya Paul <77636493+bpsmartdesign@users.noreply.github.com>
2023-07-23 23:15:30 +0100

    feat: create dropdown plugin component (#20)
    Co-authored-by: Sacha Stafyniak <sacha@digisquad.io>

--- src/plugins/components/index.ts
+++ src/plugins/components/index.ts
@@ -11,6 +11,7 @@ import buttonClose from './button-close'
 import buttonIcon from './button-icon'
 import buttonGroup from './button-group'
 import dropdown from './dropdown'
+import dropdownDivider from './dropdown-divider'
 import inputFile from './input-file'
 import focus from './focus'
 import heading from './heading'
@@ -50,6 +51,7 @@ const components = [
   buttonIcon,
   buttonGroup,
   dropdown,
+  dropdownDivider,
   inputFile,
   focus,
   heading,
