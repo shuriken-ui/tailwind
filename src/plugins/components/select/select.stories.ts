e4a5d77e4aa2bfc6713d3a66636dd3223d0ebb22
Driss Chelouati <driss.chelouati@gmail.com>
2023-09-11 13:47:25 +0100

    test: add select icon slot examples

--- src/plugins/components/select/select.stories.ts
+++ src/plugins/components/select/select.stories.ts
@@ -594,3 +594,280 @@ export const ErrorLg: Story = {
   },
 }
 // #endregion
+
+// #region Slot: icon
+export const SlotIconSm: Story = {
+  name: 'Icon slot: sm',
+  args: {
+    id: 'select',
+    label: 'Superhero',
+    size: 'sm',
+    contrast: 'default',
+    shape: 'rounded',
+    placeholder: 'Choose a hero...',
+    options: html`
+      <option value="1">Superman</option>
+      <option value="2">Batman</option>
+      <option value="3">Spiderman</option>
+      <option value="4">Ironman</option>
+    `,
+    iconSlot: html`
+      <svg
+        class="nui-select-icon-inner"
+        xmlns="http://www.w3.org/2000/svg"
+        width="32"
+        height="32"
+        viewBox="0 0 24 24"
+      >
+        <g
+          fill="none"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+        >
+          <path
+            d="M2 12a5 5 0 0 0 5 5a8 8 0 0 1 5 2a8 8 0 0 1 5-2a5 5 0 0 0 5-5V7h-5a8 8 0 0 0-5 2a8 8 0 0 0-5-2H2Z"
+          />
+          <path
+            d="M6 11c1.5 0 3 .5 3 2c-2 0-3 0-3-2Zm12 0c-1.5 0-3 .5-3 2c2 0 3 0 3-2Z"
+          />
+        </g>
+      </svg>
+    `,
+    classes: {
+      wrapper: 'w-60 max-w-xs',
+    },
+  },
+}
+
+export const SlotIconMd: Story = {
+  name: 'Icon slot: md',
+  args: {
+    id: 'select',
+    label: 'Superhero',
+    size: 'md',
+    contrast: 'default',
+    shape: 'rounded',
+    placeholder: 'Choose a hero...',
+    options: html`
+      <option value="1">Superman</option>
+      <option value="2">Batman</option>
+      <option value="3">Spiderman</option>
+      <option value="4">Ironman</option>
+    `,
+    iconSlot: html`
+      <svg
+        class="nui-select-icon-inner"
+        xmlns="http://www.w3.org/2000/svg"
+        width="32"
+        height="32"
+        viewBox="0 0 24 24"
+      >
+        <g
+          fill="none"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+        >
+          <path
+            d="M2 12a5 5 0 0 0 5 5a8 8 0 0 1 5 2a8 8 0 0 1 5-2a5 5 0 0 0 5-5V7h-5a8 8 0 0 0-5 2a8 8 0 0 0-5-2H2Z"
+          />
+          <path
+            d="M6 11c1.5 0 3 .5 3 2c-2 0-3 0-3-2Zm12 0c-1.5 0-3 .5-3 2c2 0 3 0 3-2Z"
+          />
+        </g>
+      </svg>
+    `,
+    classes: {
+      wrapper: 'w-60 max-w-xs',
+    },
+  },
+}
+
+export const SlotIconLg: Story = {
+  name: 'Icon slot: lg',
+  args: {
+    id: 'select',
+    label: 'Superhero',
+    size: 'lg',
+    contrast: 'default',
+    shape: 'rounded',
+    placeholder: 'Choose a hero...',
+    options: html`
+      <option value="1">Superman</option>
+      <option value="2">Batman</option>
+      <option value="3">Spiderman</option>
+      <option value="4">Ironman</option>
+    `,
+    iconSlot: html`
+      <svg
+        class="nui-select-icon-inner"
+        xmlns="http://www.w3.org/2000/svg"
+        width="32"
+        height="32"
+        viewBox="0 0 24 24"
+      >
+        <g
+          fill="none"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+        >
+          <path
+            d="M2 12a5 5 0 0 0 5 5a8 8 0 0 1 5 2a8 8 0 0 1 5-2a5 5 0 0 0 5-5V7h-5a8 8 0 0 0-5 2a8 8 0 0 0-5-2H2Z"
+          />
+          <path
+            d="M6 11c1.5 0 3 .5 3 2c-2 0-3 0-3-2Zm12 0c-1.5 0-3 .5-3 2c2 0 3 0 3-2Z"
+          />
+        </g>
+      </svg>
+    `,
+    classes: {
+      wrapper: 'w-60 max-w-xs',
+    },
+  },
+}
+// #endregion
+
+// #region Slot: icon float
+export const SlotIconFloatSm: Story = {
+  name: 'Icon float: sm',
+  args: {
+    id: 'select',
+    label: 'Superhero',
+    labelFloat: true,
+    size: 'sm',
+    contrast: 'default',
+    shape: 'rounded',
+    placeholder: 'Choose a hero...',
+    options: html`
+      <option value="1">Superman</option>
+      <option value="2">Batman</option>
+      <option value="3">Spiderman</option>
+      <option value="4">Ironman</option>
+    `,
+    iconSlot: html`
+      <svg
+        class="nui-select-icon-inner"
+        xmlns="http://www.w3.org/2000/svg"
+        width="32"
+        height="32"
+        viewBox="0 0 24 24"
+      >
+        <g
+          fill="none"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+        >
+          <path
+            d="M2 12a5 5 0 0 0 5 5a8 8 0 0 1 5 2a8 8 0 0 1 5-2a5 5 0 0 0 5-5V7h-5a8 8 0 0 0-5 2a8 8 0 0 0-5-2H2Z"
+          />
+          <path
+            d="M6 11c1.5 0 3 .5 3 2c-2 0-3 0-3-2Zm12 0c-1.5 0-3 .5-3 2c2 0 3 0 3-2Z"
+          />
+        </g>
+      </svg>
+    `,
+    classes: {
+      wrapper: 'w-60 max-w-xs',
+    },
+  },
+}
+
+export const SlotIconFloatMd: Story = {
+  name: 'Icon float: md',
+  args: {
+    id: 'select',
+    label: 'Superhero',
+    labelFloat: true,
+    size: 'md',
+    contrast: 'default',
+    shape: 'rounded',
+    placeholder: 'Choose a hero...',
+    options: html`
+      <option value="1">Superman</option>
+      <option value="2">Batman</option>
+      <option value="3">Spiderman</option>
+      <option value="4">Ironman</option>
+    `,
+    iconSlot: html`
+      <svg
+        class="nui-select-icon-inner"
+        xmlns="http://www.w3.org/2000/svg"
+        width="32"
+        height="32"
+        viewBox="0 0 24 24"
+      >
+        <g
+          fill="none"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+        >
+          <path
+            d="M2 12a5 5 0 0 0 5 5a8 8 0 0 1 5 2a8 8 0 0 1 5-2a5 5 0 0 0 5-5V7h-5a8 8 0 0 0-5 2a8 8 0 0 0-5-2H2Z"
+          />
+          <path
+            d="M6 11c1.5 0 3 .5 3 2c-2 0-3 0-3-2Zm12 0c-1.5 0-3 .5-3 2c2 0 3 0 3-2Z"
+          />
+        </g>
+      </svg>
+    `,
+    classes: {
+      wrapper: 'w-60 max-w-xs',
+    },
+  },
+}
+
+export const SlotIconFloatLg: Story = {
+  name: 'Icon float: lg',
+  args: {
+    id: 'select',
+    label: 'Superhero',
+    labelFloat: true,
+    size: 'lg',
+    contrast: 'default',
+    shape: 'rounded',
+    placeholder: 'Choose a hero...',
+    options: html`
+      <option value="1">Superman</option>
+      <option value="2">Batman</option>
+      <option value="3">Spiderman</option>
+      <option value="4">Ironman</option>
+    `,
+    iconSlot: html`
+      <svg
+        class="nui-select-icon-inner"
+        xmlns="http://www.w3.org/2000/svg"
+        width="32"
+        height="32"
+        viewBox="0 0 24 24"
+      >
+        <g
+          fill="none"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+        >
+          <path
+            d="M2 12a5 5 0 0 0 5 5a8 8 0 0 1 5 2a8 8 0 0 1 5-2a5 5 0 0 0 5-5V7h-5a8 8 0 0 0-5 2a8 8 0 0 0-5-2H2Z"
+          />
+          <path
+            d="M6 11c1.5 0 3 .5 3 2c-2 0-3 0-3-2Zm12 0c-1.5 0-3 .5-3 2c2 0 3 0 3-2Z"
+          />
+        </g>
+      </svg>
+    `,
+    classes: {
+      wrapper: 'w-60 max-w-xs',
+    },
+  },
+}
+// #endregion
