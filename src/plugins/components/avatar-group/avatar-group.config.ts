4c65bd2e6a6e720ebd8c220ce6141ddac083143e
Driss Chelouati <driss.chelouati@gmail.com>
2023-12-05 14:48:55 +0100

    feat(AvatarGroup): implement new config design

--- src/plugins/components/avatar-group/avatar-group.config.ts
+++ src/plugins/components/avatar-group/avatar-group.config.ts
@@ -1,6 +1,6 @@
 export const key = 'avatarGroup' as const
 
-export const config = {
+export const defaultConfig = {
   avatar: {
     rounded: 'full',
     outer: {
@@ -44,6 +44,16 @@ export const config = {
       border: {
         light: 'white',
         dark: 'muted-800',
+      },
+    },
+    text: {
+      font: {
+        family: 'sans',
+        weight: 'medium',
+        color: {
+          light: 'muted-500',
+          dark: 'muted-300',
+        },
       },
     },
     transition: {
@@ -110,82 +120,82 @@ export const config = {
   },
 }
 
-export const defaultConfig = {
-  avatarOuter: {
-    bg: 'white',
-    bgDark: 'muted-800',
-    rounded: 'full',
-    duration: '100',
-    avatar: {
-      bg: 'primary-500/20',
-      text: 'primary-500',
-    },
-  },
-  avatarCount: {
-    bg: 'white',
-    bgDark: 'muted-800',
-    rounded: 'full',
-    duration: '100',
-    inner: {
-      bg: 'muted-200',
-      bgDark: 'muted-700',
-      border: 'white',
-      borderDark: 'muted-800',
-      size: 'full',
-      rounded: 'full',
-    },
-    text: {
-      text: 'muted-500',
-      textDark: 'muted-300',
-      font: 'normal',
-    },
-  },
-  avatarGroupXXS: {
-    outer: {
-      size: '6',
-    },
-    count: {
-      size: '6',
-      text: 'xs',
-    },
-  },
-  avatarGroupXS: {
-    outer: {
-      size: '8',
-    },
-    count: {
-      size: '8',
-      text: 'sm',
-    },
-  },
-  avatarGroupSM: {
-    outer: {
-      size: '10',
-    },
-    count: {
-      size: '10',
-      text: 'sm',
-    },
-  },
-  avatarGroupMD: {
-    outer: {
-      size: '12',
-    },
-    count: {
-      size: '12',
-      text: 'lg',
-    },
-  },
-  avatarGroupLG: {
-    outer: {
-      size: '16',
-    },
-    count: {
-      size: '16',
-      text: 'xl',
-    },
-  },
-}
+// export const defaultConfig = {
+//   avatarOuter: {
+//     bg: 'white',
+//     bgDark: 'muted-800',
+//     rounded: 'full',
+//     duration: '100',
+//     avatar: {
+//       bg: 'primary-500/20',
+//       text: 'primary-500',
+//     },
+//   },
+//   avatarCount: {
+//     bg: 'white',
+//     bgDark: 'muted-800',
+//     rounded: 'full',
+//     duration: '100',
+//     inner: {
+//       bg: 'muted-200',
+//       bgDark: 'muted-700',
+//       border: 'white',
+//       borderDark: 'muted-800',
+//       size: 'full',
+//       rounded: 'full',
+//     },
+//     text: {
+//       text: 'muted-500',
+//       textDark: 'muted-300',
+//       font: 'normal',
+//     },
+//   },
+//   avatarGroupXXS: {
+//     outer: {
+//       size: '6',
+//     },
+//     count: {
+//       size: '6',
+//       text: 'xs',
+//     },
+//   },
+//   avatarGroupXS: {
+//     outer: {
+//       size: '8',
+//     },
+//     count: {
+//       size: '8',
+//       text: 'sm',
+//     },
+//   },
+//   avatarGroupSM: {
+//     outer: {
+//       size: '10',
+//     },
+//     count: {
+//       size: '10',
+//       text: 'sm',
+//     },
+//   },
+//   avatarGroupMD: {
+//     outer: {
+//       size: '12',
+//     },
+//     count: {
+//       size: '12',
+//       text: 'lg',
+//     },
+//   },
+//   avatarGroupLG: {
+//     outer: {
+//       size: '16',
+//     },
+//     count: {
+//       size: '16',
+//       text: 'xl',
+//     },
+//   },
+// }
 
 export type AvatarGroup = typeof defaultConfig
 export interface AvatarGroupPluginConfig {
