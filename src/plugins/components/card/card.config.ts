eed398af5bd15a834ee5c52755794e133e104c37
Driss Chelouati <driss.chelouati@gmail.com>
2023-12-06 20:20:45 +0100

    feat(Card): redesign config

--- src/plugins/components/card/card.config.ts
+++ src/plugins/components/card/card.config.ts
@@ -1,5 +1,105 @@
 export const key = 'card' as const
 
+const config = {
+  width: 'full',
+  color: {
+    white: {
+      background: {
+        light: 'white',
+        dark: 'muted-800',
+      },
+      border: {
+        light: 'muted-200',
+        dark: 'muted-700',
+      },
+    },
+    whiteContrast: {
+      background: {
+        light: 'white',
+        dark: 'muted-950',
+      },
+      border: {
+        light: 'muted-200',
+        dark: 'muted-800',
+      },
+    },
+    muted: {
+      background: {
+        light: 'muted-100',
+        dark: 'muted-800',
+      },
+      border: {
+        light: 'muted-200',
+        dark: 'muted-700',
+      },
+    },
+    mutedContrast: {
+      background: {
+        light: 'muted-100',
+        dark: 'muted-950',
+      },
+      border: {
+        light: 'muted-200',
+        dark: 'muted-800',
+      },
+    },
+    primary: {
+      background: {
+        light: 'primary-500/10',
+        dark: 'primary-500/10',
+      },
+      border: {
+        light: 'primary-500',
+        dark: 'primary-500',
+      },
+    },
+    info: {
+      background: {
+        light: 'info-500/10',
+        dark: 'info-500/10',
+      },
+      border: {
+        light: 'info-500',
+        dark: 'info-500',
+      },
+    },
+    success: {
+      background: {
+        light: 'success-500/10',
+        dark: 'success-500/10',
+      },
+      border: {
+        light: 'success-500',
+        dark: 'success-500',
+      },
+    },
+    warning: {
+      background: {
+        light: 'warning-500/10',
+        dark: 'warning-500/10',
+      },
+      border: {
+        light: 'warning-500',
+        dark: 'warning-500',
+      },
+    },
+    danger: {
+      background: {
+        light: 'danger-500/10',
+        dark: 'danger-500/10',
+      },
+      border: {
+        light: 'danger-500',
+        dark: 'danger-500',
+      },
+    },
+  },
+  transition: {
+    property: 'all',
+    duration: '300',
+  },
+}
+
 export const defaultConfig = {
   size: 'full',
   duration: '300',
