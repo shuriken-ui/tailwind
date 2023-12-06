2e78245daec1d06bc3f992298bf1efe08e36b361
Driss Chelouati <driss.chelouati@gmail.com>
2023-12-07 00:01:03 +0100

    feat(Dropfile): config redesign and implementation

--- src/plugins/components/fullscreen-dropfile/index.ts
+++ src/plugins/components/fullscreen-dropfile/index.ts
@@ -21,27 +21,37 @@ export default plugin.withOptions(
       ) satisfies FullscreenDropfileConfig
 
       addComponents({
+        //Wrapper
         [`.${prefix}fullscreen-dropfile`]: {
+          //Dropfile:outer
           [`.${prefix}fullscreen-dropfile-outer`]: {
-            [`@apply bg-muted-200/20 dark:bg-muted-800/20 fixed inset-0 z-40 backdrop-blur-sm transition-all hover:backdrop-blur-none`]:
+            [`@apply bg-muted-100/50 dark:bg-muted-800/20 fixed inset-0 z-40 backdrop-blur-sm transition-all hover:backdrop-blur-none`]:
               {},
           },
+          //Dropfile:inner
           [`.${prefix}fullscreen-dropfile-inner`]: {
             [`@apply fixed inset-0 z-50`]: {},
-
+            //Dropfile:container
             [`.${prefix}fullscreen-dropfile-container`]: {
               [`@apply flex h-full flex-1 items-center justify-center`]: {},
-
+              //Dropfile:content
               [`.${prefix}fullscreen-dropfile-content`]: {
-                [`@apply border-${config.border} bg-muted-200 dark:bg-muted-800 mx-auto flex h-${config.height} w-${config.width} flex-col items-center justify-center gap-6 rounded border drop-shadow-sm`]:
+                [`@apply h-${config.height} w-${config.width} mx-auto flex flex-col items-center justify-center gap-6 drop-shadow-sm`]:
                   {},
-
+                //Background
+                [`@apply bg-muted-100 dark:bg-muted-800`]: {},
+                //Border
+                [`@apply ${config.rounded} border-2 border-dashed border-${config.border.light} dark:border-${config.border.dark}`]:
+                  {},
+                //Icon
                 [`.${prefix}fullscreen-dropfile-icon`]: {
-                  [`@apply text-${config.icon.text} text-${config.icon.textSize}`]:
+                  [`@apply h-${config.icon.size} w-${config.icon.size} text-${config.icon.font.color.light} dark:text-${config.icon.font.color.dark}`]:
                     {},
                 },
+                //Label
                 [`.${prefix}fullscreen-dropfile-label`]: {
-                  [`@apply text-${config.labelTextSize}`]: {},
+                  [`@apply text-${config.label.font.size} text-muted-500 dark:text-muted-400`]:
+                    {},
                 },
               },
             },
