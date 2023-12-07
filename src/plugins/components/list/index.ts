9b9ebdaa4efd8b08c0c0df8f81020bdb78a7d191
Driss Chelouati <driss.chelouati@gmail.com>
2023-12-07 23:33:59 +0100

    feat(List): config redesign and implementation

--- src/plugins/components/list/index.ts
+++ src/plugins/components/list/index.ts
@@ -23,11 +23,17 @@ export default plugin.withOptions(
             [`@apply list-${config.ol}`]: {},
           },
           [`&.${prefix}list-base`]: {
-            [`@apply space-y-1 marker:text-${config.base.textMarker} dark:marker:text-${config.base.textMarkerDark} font-${config.base.font} text-${config.base.text} dark:text-${config.base.textDark}`]:
+            //Base
+            [`@apply space-y-1 font-${config.base.font.family}`]: {},
+            //Text
+            [`@apply text-${config.base.font.color.text.light} dark:text-${config.base.font.color.text.dark}`]:
+              {},
+            //Marker
+            [`@apply marker:text-${config.base.font.color.marker.light} dark:marker:text-${config.base.font.color.marker.dark}`]:
               {},
           },
           [`&.${prefix}list-media`]: {
-            [`@apply space-y-4 marker:text-${config.media.textMarker} dark:marker:text-${config.media.textMarkerDark}`]:
+            [`@apply space-y-4 marker:text-${config.media.marker.color.light} dark:marker:text-${config.media.marker.color.dark}`]:
               {},
             [`.${prefix}list-item`]: {
               [`@apply flex gap-2`]: {},
